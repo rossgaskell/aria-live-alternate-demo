d97d74d5d7280c5cf3c0557ba28d665be8cdb53d
Ross Gaskell <ross.gaskell@bbc.co.uk>
2021-03-24 09:09:44 +0000

    Alternate Demo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const REGION_A = 'regionA';
+const REGION_B = 'regionB';
+
+const removeWhitespace = str => str.replace(/\s+/g, '');
+
+const App = () => {
+  const [inputValue, setInputValue] = useState('This is a test announcement');
+
+  const [message, setMessage] = useState('');
+  const [liveRegion, setLiveRegion] = useState(REGION_A);
+
+  const makeAnnouncement = (newMessage) => {
+    if (removeWhitespace(message) === removeWhitespace(newMessage)) {
+      if (liveRegion === REGION_A) setLiveRegion(REGION_B);
+      if (liveRegion === REGION_B) setLiveRegion(REGION_A);
+      return;
+    }
+    setMessage(newMessage)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="form">
+          <label>
+            Message:
+            <input type="text" value={inputValue} onChange={e => setInputValue(e.target.value)} size="50"/>
+          </label>
+          <button onClick={() => makeAnnouncement(inputValue)}>Announce</button>
+        </div>
+        <div className="announcements">
+          <div aria-live="polite" id="regionA">{liveRegion === REGION_A && message}</div>
+          <div aria-live="polite" id="regionB">{liveRegion === REGION_B && message}</div>
+        </div>
       </header>
     </div>
   );
